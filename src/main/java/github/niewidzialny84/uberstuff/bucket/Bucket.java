34c0e36a80de7a357e1810fb366ca6f15ae2af89
Niewidzialny84 <niewidzialny84@vp.pl>
2020-03-30 11:32:01 +0200

    ???

--- src/main/java/github/niewidzialny84/uberstuff/bucket/Bucket.java
+++ src/main/java/github/niewidzialny84/uberstuff/bucket/Bucket.java
@@ -12,61 +12,22 @@ import java.util.Arrays;
 public class Bucket {
 
     private ItemStack bucket = new ItemStack(Material.BUCKET,1);
-    private ItemStack waterBucket = new ItemStack(Material.WATER_BUCKET,1);
-    private ItemStack lavaBucket = new ItemStack(Material.LAVA_BUCKET,1);
-    private static int maxCapacity = 10;
-    private int currentCapacity = 0;
     private static String description = ChatColor.GRAY+"Mieści dużo płynów";
-    private String status;
 
     Bucket() {
         ItemMeta bucket_meta = bucket.getItemMeta();
         bucket_meta.addEnchant(Enchantment.DIG_SPEED,1,false);
-        bucket_meta.setDisplayName(ChatColor.YELLOW +"Duże puste wiadro");
-        status = ChatColor.GRAY+"Zajęte "+ChatColor.BOLD+currentCapacity+"/"+maxCapacity;
-        bucket_meta.setLore(Arrays.asList(description,status));
+        bucket_meta.setDisplayName(ChatColor.YELLOW +"Duże wiadro");
+        bucket_meta.setLore(Arrays.asList(description));
         bucket_meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         bucket.setItemMeta(bucket_meta);
-        bucket_meta.setDisplayName(ChatColor.YELLOW +"Duże wiadro lavy");
-        lavaBucket.setItemMeta(bucket_meta);
-        bucket_meta.setDisplayName(ChatColor.YELLOW +"Duże wiadro wody");
-        waterBucket.setItemMeta(bucket_meta);
     }
 
-    public void setStatus(int currentCapacity) {
-        setBucketStatus(this.bucket,currentCapacity);
-        setBucketStatus(this.lavaBucket,currentCapacity);
-        setBucketStatus(this.waterBucket,currentCapacity);
-    }
-
-    public static ItemStack setBucketStatus(ItemStack bucket, int currentCapacity) {
-        ItemMeta bucket_meta = bucket.getItemMeta();
-        String status = ChatColor.GRAY+"Zajęte "+ChatColor.BOLD+currentCapacity+"/"+maxCapacity;
-        bucket_meta.setLore(Arrays.asList(description,status));
-        bucket.setItemMeta(bucket_meta);
-        return bucket;
-    }
-
-    public static boolean isCorrectBucket(ItemStack a,ItemStack b) {
-        ItemMeta aMeta = a.getItemMeta();
-        ItemMeta bMeta = b.getItemMeta();
-
-        if(aMeta.getLore().get(0).equals(bMeta.getLore().get(0))) {
-            return true;
-        }
-
-        return false;
+    public boolean isCorrectBucket(ItemStack a) {
+        return a.getItemMeta().getLore().get(0) != null && a.getItemMeta().getLore().get(0).equals(bucket.getItemMeta().getLore().get(0));
     }
 
     public ItemStack getBucket() {
         return bucket;
-    }
-
-    public ItemStack getWaterBucket() {
-        return waterBucket;
-    }
-
-    public ItemStack getLavaBucket() {
-        return lavaBucket;
     }
 }
--- src/main/java/github/niewidzialny84/uberstuff/bucket/BucketListener.java
+++ src/main/java/github/niewidzialny84/uberstuff/bucket/BucketListener.java
@@ -1,6 +1,8 @@
 package github.niewidzialny84.uberstuff.bucket;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBucketFillEvent;
@@ -18,20 +20,19 @@ public class BucketListener implements Listener {
 
     @EventHandler
     public void playerJoin(PlayerJoinEvent e) {
-        e.getPlayer().getInventory().addItem(bucket.getBucket(),bucket.getLavaBucket(),bucket.getWaterBucket());
+        e.getPlayer().getInventory().addItem(bucket.getBucket());
     }
 
 
     @EventHandler
     public void playerFillClick(PlayerBucketFillEvent e) {
-        ItemStack hand = e.getPlayer().getInventory().getItemInMainHand();
-        if(Bucket.isCorrectBucket(hand,bucket.getBucket())) {
-            System.out.println("puste");
-        } else if (Bucket.isCorrectBucket(hand,bucket.getLavaBucket())) {
-            System.out.println("lava");
-        } else if (Bucket.isCorrectBucket(hand,bucket.getWaterBucket())) {
-            System.out.println("woda");
+        Block block = e.getBlock();
+        if((block.getType().equals(Material.WATER) || block.getType().equals(Material.LAVA))) {
+            if (bucket.isCorrectBucket(e.getPlayer().getInventory().getItemInMainHand())) {
+                e.getPlayer().getInventory().setItemInMainHand(bucket.getBucket());
+            } else if (bucket.isCorrectBucket(e.getPlayer().getInventory().getItemInOffHand())) {
+                e.getPlayer().getInventory().setItemInOffHand(bucket.getBucket());
+            }
         }
-
     }
 }
