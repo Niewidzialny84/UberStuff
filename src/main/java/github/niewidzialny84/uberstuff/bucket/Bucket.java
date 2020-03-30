b5c94abd8da7abd9cf8590b8355acf666bb6371c
Niewidzialny84 <niewidzialny84@vp.pl>
2020-03-30 13:59:51 +0200

    Yes but actually no

--- src/main/java/github/niewidzialny84/uberstuff/bucket/Bucket.java
+++ src/main/java/github/niewidzialny84/uberstuff/bucket/Bucket.java
@@ -1,26 +1,42 @@
 package github.niewidzialny84.uberstuff.bucket;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 
 import java.util.Arrays;
 
 public class Bucket {
 
     private ItemStack bucket = new ItemStack(Material.BUCKET,1);
-    private static String description = ChatColor.GRAY+"Mieści dużo płynów";
+    private final String description = ChatColor.GRAY+"Mieści dużo płynów";
+    private ShapedRecipe recipe;
+    private NamespacedKey key;
 
-    Bucket() {
+
+    Bucket(Plugin plugin) {
         ItemMeta bucket_meta = bucket.getItemMeta();
         bucket_meta.addEnchant(Enchantment.DIG_SPEED,1,false);
         bucket_meta.setDisplayName(ChatColor.YELLOW +"Duże wiadro");
         bucket_meta.setLore(Arrays.asList(description));
         bucket_meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         bucket.setItemMeta(bucket_meta);
+
+        key = new NamespacedKey(plugin,"SuperBucket");
+        recipe = new ShapedRecipe(key,bucket);
+        recipe.shape("   ","axa"," a ");
+        recipe.setIngredient('a',Material.IRON_BLOCK);
+        recipe.setIngredient('x',Material.ENDER_EYE);
+        recipe.setGroup("UberStuffSpecial");
+
+        Bukkit.addRecipe(recipe);
     }
 
     public boolean isCorrectBucket(ItemStack a) {
@@ -34,5 +50,13 @@ public class Bucket {
 
     public ItemStack getBucket() {
         return bucket;
+    }
+
+    public ShapedRecipe getRecipe() {
+        return recipe;
+    }
+
+    public NamespacedKey getKey() {
+        return key;
     }
 }
--- src/main/java/github/niewidzialny84/uberstuff/bucket/BucketListener.java
+++ src/main/java/github/niewidzialny84/uberstuff/bucket/BucketListener.java
@@ -3,10 +3,15 @@ package github.niewidzialny84.uberstuff.bucket;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Dispenser;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Levelled;
 import org.bukkit.block.data.Waterlogged;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.CauldronLevelChangeEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
@@ -14,15 +19,16 @@ import org.bukkit.plugin.Plugin;
 
 public class BucketListener implements Listener {
 
-    private Bucket bucket = new Bucket();
+    private Bucket bucket;
 
     public BucketListener(Plugin plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
+        bucket = new Bucket(plugin);
     }
 
     @EventHandler
     public void playerJoin(PlayerJoinEvent e) {
-        e.getPlayer().getInventory().addItem(bucket.getBucket());
+        e.getPlayer().discoverRecipe(bucket.getKey());
     }
 
 
@@ -33,6 +39,7 @@ public class BucketListener implements Listener {
         if (bucket.isCorrectBucket(e.getPlayer().getInventory().getItemInMainHand()) || bucket.isCorrectBucket(e.getPlayer().getInventory().getItemInOffHand())) {
             if(e.getItemStack().equals(new ItemStack(Material.MILK_BUCKET,1))) {
                 e.setCancelled(true);
+                e.getPlayer().updateInventory();
                 return;
             }
 
@@ -49,7 +56,46 @@ public class BucketListener implements Listener {
             if((block.getType().equals(Material.WATER) || block.getType().equals(Material.LAVA))) {
                 e.setCancelled(true);
                 block.setType(Material.AIR);
+                if(e.getPlayer().isSneaking()) {
+                    for(int x = -1; x<= 1;x++) {
+                        for(int y = -1; y<=1; y++) {
+                            for(int z = -1; z<=1; z++) {
+                                Block tmpBlock = block.getLocation().add(x,y,z).getBlock();
+                                if(tmpBlock.getType().equals(Material.LAVA) ||tmpBlock.getType().equals(Material.WATER)) {
+                                    tmpBlock.setType(Material.AIR);
+                                }
+                            }
+                        }
+                    }
+                }
             }
+        }
+    }
+
+   @EventHandler
+    public void cauldronFill(CauldronLevelChangeEvent e) {
+        if(e.getEntity() instanceof Player) {
+            Player player = ((Player) e.getEntity()).getPlayer();
+            Block block = e.getBlock();
+            BlockData blockData = block.getBlockData();
+            if (e.getReason().equals(CauldronLevelChangeEvent.ChangeReason.BUCKET_FILL) && (bucket.isCorrectBucket(player.getInventory().getItemInMainHand()) || bucket.isCorrectBucket(player.getInventory().getItemInOffHand()))) {
+                e.setCancelled(true);
+                if(blockData instanceof Levelled && block.getType().equals(Material.CAULDRON)) {
+                    Levelled cauldronData = (Levelled) blockData;
+                    if(cauldronData.getLevel() == cauldronData.getMaximumLevel()) {
+                        cauldronData.setLevel(0);
+                        block.setBlockData(cauldronData);
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void dispenserFill(BlockDispenseEvent e) {
+        if (bucket.isCorrectBucket(e.getItem())) {
+            Dispenser dispenser = (Dispenser) e.getBlock().getState();
+            e.setCancelled(true);
         }
     }
 }
