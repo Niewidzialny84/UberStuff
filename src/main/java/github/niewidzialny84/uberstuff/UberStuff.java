08728e5b897d132451323ec61e273341a5b5c276
Niewidzialny84 <niewidzialny84@vp.pl>
2020-04-11 19:06:02 +0200

    Config prep

--- src/main/java/github/niewidzialny84/uberstuff/UberStuff.java
+++ src/main/java/github/niewidzialny84/uberstuff/UberStuff.java
@@ -1,6 +1,7 @@
 package github.niewidzialny84.uberstuff;
 
 import github.niewidzialny84.uberstuff.bucket.BucketListener;
+import github.niewidzialny84.uberstuff.voidfall.PlayerFall;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UberStuff extends JavaPlugin {
@@ -8,7 +9,7 @@ public final class UberStuff extends JavaPlugin {
     @Override
     public void onEnable() {
         new BucketListener(this);
-
+        new PlayerFall(this);
     }
 
     @Override
